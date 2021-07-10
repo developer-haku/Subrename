e0c3f467a983b4998fe20e667e423a60cbe03557
Liuming Chen <51524534+developer-haku@users.noreply.github.com>
2021-07-09 23:49:24 -0400

    build: tailwind CSS setup (#2)

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import colors from 'tailwindcss/colors';
+
+module.exports = {
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        sky: colors.sky,
+        cyan: colors.cyan,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
